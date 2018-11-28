03f2caaf183e2f204a2d6b4f9625505d8cac5891
Spartak Bughdaryan <spartakbughdaryan@Spartaks-MacBook-Air.local>
2018-11-28 15:34:01 +0400

    Add dashbord, can now see my books, only if logged in

--- book_store_front/src/components/Dashboard.jsx
+++ book_store_front/src/components/Dashboard.jsx
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
+import Auth from '../modules/Auth';
 
 class Dashboard extends Component {
     constructor(){
         super();
+        this.state = {
+            myBooks:null,
+            booksLoaded: false
+        };
+    }
+
+    async componentDidMount(){
+        try{
+            const res = await fetch('/profile',{
+                method:'GET',
+                headers:{
+                    token:Auth.getToken(),
+                    'Authorization': `Token ${Auth.getToken()}`
+                }
+            })
+            const resJson = await res.json();
+            this.setState({
+                myBooks: resJson.books,
+                booksLoaded: true
+            });
+        }catch (err){
+            console.log(err);
+        }
     }
 
     render() {
         return (
             <div className='dash'>
-                <p>I LOGED IN</p>
+                {(this.state.booksLoaded)? this.state.myBooks.map(book => {
+                    return <h1 key={book.id}>{book.title}</h1>
+                }):<h1>Loading...</h1> }
             </div>
         );
     }
